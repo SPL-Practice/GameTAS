6b4d5961db2479ff3f7bc443bfe52762d4bfd341
Alexxx180 <alexx120702@gmail.com>
2023-01-06 23:24:36 +0300

    Menu start level dependency fixed

--- gametas/assets/scripts/menu/main.gd
+++ gametas/assets/scripts/menu/main.gd
@@ -5,7 +5,7 @@ func _ready():
 
 func _on_start_pressed():
 	# warning-ignore:return_value_discarded
-	get_tree().change_scene("res://assets/scenes/levels/office/start.tscn")
+	get_tree().change_scene("res://assets/scenes/levels/start.tscn")
 
 
 func _on_exit_pressed():
