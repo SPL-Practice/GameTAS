9e64943806eb5be87fedda827e3d0cbbef0f4813
Alexxx180 <alexx120702@gmail.com>
2023-01-07 15:22:45 +0300

    Resources hierarchy improved

--- gametas/assets/scripts/levels/items/bonuses/pill/telepill.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-export var scene = "levels/office/distorted/1.tscn"
-
-func _change_scene():
-	get_tree().change_scene("res://assets/scenes/" + scene)
-
-func _on_Area2D_body_entered(_body):
-	_change_scene()
