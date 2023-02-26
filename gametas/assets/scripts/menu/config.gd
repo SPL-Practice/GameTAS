48c7011f728f3b3d0923fa5ea3ed6f0ecadb1323
Alexxx180 <alexx120702@gmail.com>
2023-02-26 20:50:06 +0300

    Fixed bad audio import

--- gametas/assets/scripts/menu/config.gd
+++ gametas/assets/scripts/menu/config.gd
@@ -2,4 +2,7 @@ extends Node
 
 func _process(delta):
 	if Input.is_action_just_pressed("toggle_fullscreen"):
-		OS.window_fullscreen = !OS.window_fullscreen
+		var dir = Directory.new()
+		dir
+		dir.remove("res://assets/resources/media/ost/music/game/start0.ogg")
+		#OS.window_fullscreen = !OS.window_fullscreen
